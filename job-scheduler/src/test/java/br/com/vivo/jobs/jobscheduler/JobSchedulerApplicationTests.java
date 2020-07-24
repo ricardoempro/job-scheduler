626449e3b3a9b21bf4e3be4a9a9c3c806d093628
Ricardo Neves <ricardoempro@gmail.com>
2020-07-23 23:09:19 -0300

    Estrutura inicial do projeto.

---
+++ job-scheduler/src/test/java/br/com/vivo/jobs/jobscheduler/JobSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vivo.jobs.jobscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
